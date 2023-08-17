d48662678ecf8f7ccde3ec85dd2818e6e25b4cc5
Fabion Burke <fabion.burke@dewr.gov.au>
2023-08-17 13:47:34 +1000

    Tweak test

---
+++ WebAPITest1/Fakes/FakeOptionsSnapshot.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPITest1.UnitTests.Fakes
+{
+    public class FakeOptionsSnapshot<TOptions> : IOptionsSnapshot<TOptions> where TOptions : class
+    {
+        private readonly TOptions options;
+
+        public FakeOptionsSnapshot(TOptions options)
+        {
+            this.options = options;
+        }
+
+        public TOptions Value => options;
+
+        public TOptions Get(string? name)
+        {
+            return Value;
+        }
+    }
+}
